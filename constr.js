7727343ff1c22c573addf7787cadff888dbd548b
KasoziJoel <joelmet256@gmail.com>
2025-10-08 08:16:27 +0300

    Add files via upload

---
+++ constr.js
@@ -0,0 +1,16 @@
+
+class student{
+    constructor(fullname,yob,accessNo,phone,isRegistered){
+        this.fullname=fullname;
+        this.yob=yob;
+        this.accessNo=accessNo;
+        this.phone=phone;
+        this.isRegistered=isRegistered};
+}
+
+
+// studentAge = function(){
+//     //AGE = current year - student year of birth
+// }
+
+const Student1 = new student("Sn fll", 2003, "M25B13051", 773837600, true);
