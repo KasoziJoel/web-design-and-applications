d08caf9f0e2d88e5bf97907b59a05a49948362f4
KasoziJoel <joelmet256@gmail.com>
2025-10-13 10:58:38 +0300

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,24 @@
+
+// const student = {
+//     fullname: "Jo Go",
+//     yob: 2000,
+//     regno: "M20B14/001",
+//     gender:"M",
+//     phone: "90876668"
+    
+// };
+// console.log(student.regno);
+
+// const student2 = {
+//     fullname: "Jo Go",
+//     yob: 2000,
+//     regno: "M20B14/001",
+//     gender:"M",
+//     phone: "90876668",
+//     isRegistered: false
+
+// };
+
+// console.log(student2.registered, student2.phone, student.fullname)
+
+
